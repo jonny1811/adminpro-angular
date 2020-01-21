d1d07e7d9fbd63909e21ecd42254c50d6421c428
Jonny Lezcano <jonnyda1811@gmail.com>
2020-01-20 23:15:01 -0300

    Agregado progress con @input, @output y @viewChild

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,22 +1,26 @@
-import { SharedModule } from './../shared/shared.module';
-// Modulos
 import { NgModule } from '@angular/core';
+import { PagesRoutes } from './pages.routes';
+// Modulos
+import { SharedModule } from './../shared/shared.module';
+// Temporal
+import { FormsModule } from '@angular/forms';
 // Componentes Padres
 import { PagesComponent } from './pages.component';
 // Componentes Hijos
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
-// Rutas Hijas
-import { PagesRoutes } from './pages.routes';
 
+// Temporal
+import { IncrementadorComponent } from './../components/incrementador/incrementador.component';
 
 @NgModule({
     declarations: [
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
-        Graficas1Component
+        Graficas1Component,
+        IncrementadorComponent
     ],
     exports: [
         DashboardComponent,
@@ -25,7 +29,8 @@ import { PagesRoutes } from './pages.routes';
     ],
     imports: [
         SharedModule,
-        PagesRoutes
+        PagesRoutes,
+        FormsModule
     ]
 })
 
